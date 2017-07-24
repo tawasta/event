b5de1e67082789d0efc0861e7007631e7acf118b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-07-24 16:51:53 +0300

    Improve colums width

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.0.7.5',
+    'version': '10.0.0.7.6',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
