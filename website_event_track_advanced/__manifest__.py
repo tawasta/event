228a9be2c01cf8ce08afadb0fca64bcb310bc32b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-07-28 12:37:44 +0300

    Add a new contact person panel

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.0.8.0',
+    'version': '10.0.0.8.2',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
