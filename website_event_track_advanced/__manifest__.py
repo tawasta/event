d7f48550e643e2075bd5eb16944a0d52351d6ad2
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-08-10 10:34:57 +0300

    Improve invoice address layout

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.0.8.5',
+    'version': '10.0.0.8.6',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
