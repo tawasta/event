f7afb333fa3c4a274055531a0f7668ec8957c0c0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-08-11 15:42:51 +0300

    Refactor website agenda

--- website_event_track_advanced/controllers/__init__.py
+++ website_event_track_advanced/controllers/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
-from . import event_track
+from . import event_track_agenda
+from . import event_track_proposal
---
+++ website_event_track_advanced/controllers/event_track_agenda.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import http, fields
+from odoo.http import request
+
+# 4. Imports from Odoo modules (rarely, and only if necessary):
+from odoo.addons.website_event_track.controllers.main import WebsiteEventTrackController
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports (One per line sorted and splitted in
+
+
+class WebsiteEventTrackController(WebsiteEventTrackController):
+    pass
