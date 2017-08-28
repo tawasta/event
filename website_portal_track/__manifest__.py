263f823d186f02dad7f193d7117d479c4ddeab32
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-08-28 09:54:19 +0300

    Add a missing dependency

--- website_portal_track/__manifest__.py
+++ website_portal_track/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks portal',
     'summary': 'Event tracks in website portal',
-    'version': '10.0.0.3.0',
+    'version': '10.0.0.3.1',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
@@ -34,6 +34,7 @@
         'bin': [],
     },
     'depends': [
+        'portal',
         'website_event_track_advanced',
     ],
     'data': [
