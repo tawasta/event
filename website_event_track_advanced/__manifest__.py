f0aaf8cb7e2eaddb7ab5efcfc36e19f9250a235c
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-08-28 15:24:25 +0300

    Add editable fields

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.0.10.12',
+    'version': '10.0.0.10.13',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
