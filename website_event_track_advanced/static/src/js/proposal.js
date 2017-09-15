8b8a5d8dd72fc5772831e38f671c683a9ab81c77
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-09-15 15:22:10 +0300

    Improve UI

--- website_event_track_advanced/static/src/js/proposal.js
+++ website_event_track_advanced/static/src/js/proposal.js
@@ -136,4 +136,15 @@ odoo.define('proposal', function (require) {
         });
     })
 
+    // Suggest the contact as a speaker
+    $('#track-application-contract-input-div input').change(function(){
+        var changed_field = $(this).attr('name').substr(8);
+        var corresponding_field_name = 'speaker_' + changed_field + '[0]';
+        var corresponding_field = $("input[name='" + corresponding_field_name + "']" );
+
+        if(corresponding_field.val() == ''){
+            corresponding_field.val($(this).val())
+        }
+    });
+
 });
