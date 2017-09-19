e4bfa8a791e799e24c951dda488773d1ba343cf5
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-09-19 16:06:14 +0300

    Improve modal dialogs

--- website_portal_track/static/src/js/event_track_list.js
+++ website_portal_track/static/src/js/event_track_list.js
@@ -1,7 +1,15 @@
 $(function() {
 
     // Turn the whole tr into a link
-    $('#event_track_list tr').click(function() {
+    $('#event_track_list .td-submit').click(function() {
         window.location = $(this).data("href");
+    });
+
+    // Confirmation dialog
+    $('#track-confirm-action').on('show.bs.modal', function(e) {
+        $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+
+        $(this).find('.modal-track-action').text( $(e.relatedTarget).data('action') );
+        $(this).find('.modal-track-title').text( $(e.relatedTarget).data('title') );
     });
 });
