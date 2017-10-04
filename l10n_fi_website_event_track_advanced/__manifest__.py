dbe019f48c70b691dfd644178fe9a1a918a64206
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-10-04 15:44:11 +0300

    Fix a duplication

--- l10n_fi_website_event_track_advanced/__manifest__.py
+++ l10n_fi_website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Finnish localization for advanced event tracks',
     'summary': 'Finnish localization for advanced event tracks',
-    'version': '10.0.0.3.3',
+    'version': '10.0.0.3.4',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
