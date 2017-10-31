24434a263a75f17f5a27b93f06d3c548003d9523
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-10-31 12:50:00 +0200

    Add and empty default selection for type

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.1.1.9',
+    'version': '10.0.1.1.10',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
