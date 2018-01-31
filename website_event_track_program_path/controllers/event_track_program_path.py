4813be5cecb07af39189bf9db255eb801c9812a5
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-01-31 10:23:57 +0200

    Add share buttons

--- website_event_track_program_path/controllers/event_track_program_path.py
+++ website_event_track_program_path/controllers/event_track_program_path.py
@@ -5,6 +5,7 @@ from odoo import http, fields
 from odoo.http import request
 
 from odoo.addons.website_event_track.controllers.main import WebsiteEventTrackController
+from odoo.tools import html2plaintext
 
 
 class WebsiteEventTrackController(WebsiteEventTrackController):
@@ -22,8 +23,6 @@ class WebsiteEventTrackController(WebsiteEventTrackController):
         program = EventTrackProgramPath.search([
             ('partner_id.id', '=', request.uid)
         ], limit=1)
-
-        print program
 
         # Program not found. Create a new
         if not program:
@@ -47,3 +46,29 @@ class WebsiteEventTrackController(WebsiteEventTrackController):
             program.track_ids = [(4, track_id, False)]
 
         return 200
+
+
+    # Program path listing
+    @http.route([
+        '''/event/<model("event.event", "[('show_tracks','=',1)]"):event>/program-path/<model("event.track.program.path"):program>'''
+    ], type='http', auth="public", website=True)
+    def event_track_program_path(self, event, program, **post):
+        searches = {}
+
+        base_url = request.env['ir.config_parameter'].get_param('web.base.url')
+
+        values = {
+            'event': event,
+            'main_object': event,
+            'tracks': program.track_ids,
+            'tags': event.tracks_tag_ids,
+            'tag_id': None,
+            'searches': searches,
+            'html2plaintext': html2plaintext,
+            'program': program,
+            'base_url': base_url,
+        }
+        return request.render(
+            "website_event_track_program_path.program_path_list",
+            values,
+        )
