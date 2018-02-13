674e97258c212fb6c0bc7c45fe011e4d1cde9e8a
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-02-13 13:22:01 +0200

    Add new module for  adding images for tracks

---
+++ website_event_track_image/models/event_track.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class EventTrack(models.Model):
+
+    _inherit = 'event.track'
+
+    image = fields.Binary(
+        string='Image',
+    )
