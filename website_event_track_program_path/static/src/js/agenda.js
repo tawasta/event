c2e9af155f9a9a683790da91adbef157d9b6c734
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-03-29 14:02:21 +0300

    Fix a condition

--- website_event_track_program_path/static/src/js/agenda.js
+++ website_event_track_program_path/static/src/js/agenda.js
@@ -46,7 +46,7 @@ odoo.define('agenda-program', function (require) {
 
     $(function() {
         // Info box (guidance)
-        if($('#track-my-program-path').length && $('#track-my-program-path').is(":hidden")){
+        if(not $('#track-my-program-path').length || $('#track-my-program-path').is(":hidden")){
             toastr.options = {
               "closeButton": false,
               "debug": false,
