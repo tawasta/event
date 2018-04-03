982f22d71c67258e89fa512f7438dc37345a26f0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-04-03 14:20:34 +0300

    Add a timeout to get translations working

--- website_event_track_program_path/static/src/js/agenda.js
+++ website_event_track_program_path/static/src/js/agenda.js
@@ -45,30 +45,34 @@ odoo.define('agenda-program', function (require) {
         })
 
     $(function() {
-        // Info box (guidance)
-        if(!$('#track-my-program-path').length || $('#track-my-program-path').is(":hidden")){
+        function showProgramPathInfo () {
+            // Info box (guidance)
+            if(!$('#track-my-program-path').length || $('#track-my-program-path').is(":hidden")){
 
-            toastr.options = {
-              "closeButton": false,
-              "debug": false,
-              "newestOnTop": true,
-              "progressBar": true,
-              "positionClass": "toast-top-right",
-              "preventDuplicates": true,
-              "timeOut": "0",
-              "showEasing": "swing",
-              "hideEasing": "linear",
-              "showMethod": "fadeIn",
-              "hideMethod": "fadeOut"
+                toastr.options = {
+                  "closeButton": false,
+                  "debug": false,
+                  "newestOnTop": true,
+                  "progressBar": true,
+                  "positionClass": "toast-top-right",
+                  "preventDuplicates": true,
+                  "timeOut": "0",
+                  "showEasing": "swing",
+                  "hideEasing": "linear",
+                  "showMethod": "fadeIn",
+                  "hideMethod": "fadeOut"
+                }
+
+                var info_title = '<p>' + _t('Create your own program path') + '!</p>';
+                var info_msg = '<p>' + _t('Login to create your own program path by selecting presentations from the program by pressing the star symbol') + ' <span class="fa fa-star-o" />.</p>';
+                info_msg += '<p>' + _t('You can also share your program path with others.') + '</p>';
+                info_msg += '<p>' + _t('Nb! creating a program path will not mandate a participation for the selected presentations and also will not reserve a seat for you') + '.</p>';
+
+                toastr.info(info_msg, info_title);
             }
-
-            var info_title = '<p>' + _t('Create your own program path') + '!</p>';
-            var info_msg = '<p>' + _t('Login to create your own program path by selecting presentations from the program by pressing the star symbol') + ' <span class="fa fa-star-o" />.</p>';
-            info_msg += '<p>' + _t('You can also share your program path with others.') + '</p>';
-            info_msg += '<p>' + _t('Nb! creating a program path will not mandate a participation for the selected presentations and also will not reserve a seat for you') + '.</p>';
-
-            toastr.info(info_msg, info_title);
         }
-    });
 
+        // Run with timeout to get translations working
+        setTimeout(showProgramPathInfo, 500);
+    });
 });
