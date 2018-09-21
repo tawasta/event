f15113baf21dcb6ace9104d4d6340f5a4198fc4f
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-09-21 14:10:33 +0300

    Add a tag filter

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.1.13.8',
+    'version': '10.0.1.13.9',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
