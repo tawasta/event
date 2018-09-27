8fa3e7e740a484d8807d46278e74f07affb38465
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-09-27 10:57:00 +0300

    hide scrollbar

--- website_event_track_advanced/static/src/js/scrollbar.js
+++ website_event_track_advanced/static/src/js/scrollbar.js
@@ -1,19 +1,19 @@
-	odoo.define('website_event_track_advanced.agenda_day_content', function (require) {
-	"use strict";
+// 	odoo.define('website_event_track_advanced.agenda_day_content', function (require) {
+// 	"use strict";
 
-	var core = require('web.core');
+// 	var core = require('web.core');
 
-	$(function(){
-		$(".wrapper1").scroll(function(){
-        $(".wrapper2")
-            .scrollLeft($(".wrapper1").scrollLeft());
-            console.log('Päästäänkö tähän?')
-    });
-    $(".wrapper2").scroll(function(){
-    	console.log("Meneekö edes toisen function sisälle?")
-        $(".wrapper1")
-            .scrollLeft($(".wrapper2").scrollLeft());
-            console.log("Entäs tänne?")
-    });
-});
-});
+// 	$(function(){
+// 		$(".wrapper1").scroll(function(){
+//         $(".wrapper2")
+//             .scrollLeft($(".wrapper1").scrollLeft());
+//             console.log('Päästäänkö tähän?')
+//     });
+//     $(".wrapper2").scroll(function(){
+//     	console.log("Meneekö edes toisen function sisälle?")
+//         $(".wrapper1")
+//             .scrollLeft($(".wrapper2").scrollLeft());
+//             console.log("Entäs tänne?")
+//     });
+// });
+// });
