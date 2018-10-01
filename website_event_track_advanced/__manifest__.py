88bce3d5dd82763a619163fff41fafa40fd3a2d3
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-10-01 16:02:24 +0300

    [ADD] Increased versioning (merge acceptance to other changes)

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.1.14.16',
+    'version': '10.0.1.15.0',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
