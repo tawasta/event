1d5d1c07e665a8874d4ccdc750665e75acc4768e
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-11-07 10:19:11 +0200

    [ADD] Added translations

--- website_event_registration_profession/__manifest__.py
+++ website_event_registration_profession/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Professions for event registrations',
     'summary': 'Adds profession to event registrations',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.1.1',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
--- website_event_registration_profession/static/src/js/registration.js
+++ website_event_registration_profession/static/src/js/registration.js
@@ -1,8 +1,12 @@
 odoo.define('website_event_registration_profession.registration', function (require) {
     "use strict";
 
+    var _t = require('web.core')._t;
+
     $(function() {
 
-        $('.profession-select').select2();
+        $('.profession-select').select2({
+            placeholder: _t("Select a profession")
+        });
     });
 });
