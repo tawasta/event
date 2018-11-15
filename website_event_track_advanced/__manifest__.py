4b0262badef08ef03ee1607f84116aaa9076501e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-11-15 15:35:58 +0200

    Improve customizability

--- website_event_track_advanced/__manifest__.py
+++ website_event_track_advanced/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Advanced event tracks',
     'summary': 'An advanced event tracks process',
-    'version': '10.0.1.16.5',
+    'version': '10.0.1.16.6',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
