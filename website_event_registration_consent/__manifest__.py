b85540ff53cb4566a58f649389872b9a0ce22092
aleksi <aleksi.savijoki@tawasta.fi>
2018-11-28 16:31:44 +0200

    [ADD] Add translations and fix ticket layout

--- website_event_registration_consent/__manifest__.py
+++ website_event_registration_consent/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Event Registration Consent',
     'summary': 'Adds event registration consent',
-    'version': '10.0.0.1.0',
+    'version': '10.0.1.0.0',
     'category': 'Events',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
