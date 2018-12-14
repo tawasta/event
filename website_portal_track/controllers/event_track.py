f3a612ea3800033cd41872555c270fe1e79a50c8
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-12-14 12:39:37 +0200

    [ADD] Dirty fix for saving einvoice information

--- website_portal_track/controllers/event_track.py
+++ website_portal_track/controllers/event_track.py
@@ -11,6 +11,7 @@ from odoo.http import request
 from odoo.exceptions import AccessError
 
 # 4. Imports from Odoo modules:
+from odoo.addons.website_portal.controllers.main import website_account
 from odoo.addons.website_event_track_advanced.controllers.event_track_proposal import WebsiteEventTrackController
 
 # 5. Local imports in the relative form:
@@ -19,7 +20,7 @@ from odoo.addons.website_event_track_advanced.controllers.event_track_proposal i
 _logger = logging.getLogger(__name__)
 
 
-class WebsiteEventTrack(WebsiteEventTrackController):
+class WebsiteEventTrack(website_account):
 
     # Add track count to account main menu
     @http.route()
@@ -172,6 +173,45 @@ class WebsiteEventTrack(WebsiteEventTrackController):
                 speakers.append((4, new_speaker.id))
 
             track_values['speaker_ids'] = speakers
+
+            # TODO: remove this
+            if post.get('organizer_business_id') and post.get(
+                    'organizer_business_id') != 'false':
+                values['workshop_organizer']['business_id'] = post.get(
+                    'organizer_business_id')
+
+            if post.get('organizer_edicode') and post.get(
+                    'organizer_edicode') != 'false':
+                values['workshop_organizer']['edicode'] = post.get(
+                    'organizer_edicode')
+
+            if post.get('organizer_einvoice_operator') and post.get(
+                    'organizer_einvoice_operator') != 'false':
+                einvoice_operator_module = request.env[
+                    'res.partner.operator.einvoice']
+                operator_name = post.get('organizer_einvoice_operator')
+
+                existing_operator = einvoice_operator_module.sudo().search([
+                    ('name', 'ilike', operator_name)
+                ], limit=1)
+
+                # Create an einvoice operator if one doesn't exist
+                if not existing_operator:
+                    operator_identifier = False
+                    if post.get(
+                            'organizer_einvoice_operator_identifier') and post.get(
+                            'organizer_einvoice_operator_identifier') != 'false':
+                        operator_identifier = post.get(
+                            'organizer_einvoice_operator_identifier')
+
+                    existing_operator = einvoice_operator_module.sudo().create(
+                        {'name': operator_name,
+                         'identifier': operator_identifier}
+                    )
+
+                values['workshop_organizer'][
+                    'einvoice_operator'] = existing_operator.id
+            # TODO: EOF "remove this"
 
             # Add workshop organization
             workshop_organizer = False
