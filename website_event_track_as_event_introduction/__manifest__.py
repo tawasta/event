320d3466dc596ea1431e207f029734f121b99aee
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-01-04 14:59:36 +0200

    [ADD] A new module for using a track as event description

---
+++ website_event_track_as_event_introduction/__manifest__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2019 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': 'Website event track as event description',
+    'summary': 'Show a track as event description',
+    'version': '10.0.1.0.0',
+    'category': 'Events',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [
+        ],
+        'bin': [],
+    },
+    'depends': [
+        'website_event_track',
+        'website_event_track_advanced',
+    ],
+    'data': [
+        'views/event_event_form.xml',
+        'views/website_event_description_full.xml',
+    ],
+    'demo': [
+    ],
+}
---
+++ website_event_track_as_event_introduction/models/event_event.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    event_introduction_track_id = fields.Many2one(
+        comodel_name='event.track',
+        name='Event introduction',
+        help='Use this presentation as event introduction',
+    )
