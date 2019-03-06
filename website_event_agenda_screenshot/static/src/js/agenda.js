7400fd2143b1b7fa1ba10f738d734cfd8fadb08f
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-03-06 14:10:33 +0000

    [IMP] image download cross-browser compatibility

--- website_event_agenda_screenshot/static/src/js/agenda.js
+++ website_event_agenda_screenshot/static/src/js/agenda.js
@@ -6,7 +6,13 @@ odoo.define('website_event_agenda_screenshot.html2canvas', function (require) {
             var target = container.find('.agenda-day-content:first')[0];
 
             html2canvas(target).then(canvas => {
-                window.open(canvas.toDataURL("image/png"));
+                // Create a link and "click" it
+                var link = document.createElement('a');
+                link.href = canvas.toDataURL("image/png");
+                link.download = 'event_schedule.png';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
             });
         });
     });
