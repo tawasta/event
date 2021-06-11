c47bfd89527c29b4361735299b15eb7f79f293ba
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-06-11 08:04:49 +0300

    Modified function

--- event_batch/models/event_registration.py
+++ event_batch/models/event_registration.py
@@ -17,15 +17,20 @@ class EventRegistration(models.Model):
                     "mobile": registration.partner_id.phone,
                     "birth_date": "1995-01-01",
                 }
-                create_student = self.env["op.student"].sudo().create(vals)
+                student_batch_vals = {
+                    "batch_id": registration.event_id.batch_id.id,
+                    "event_id": registration.event_id.id,
+                }
+                is_student = self.env["op.student"].sudo().search([
+                    ('partner_id', '=', registration.partner_id.id)
+                ])
+                if is_student:
+                    student_batch_vals.update({"student_id": is_student.id})
+                else:
+                    create_student = self.env["op.student"].sudo().create(vals)
+                    student_batch_vals.update({"student_id": create_student.id})
 
-                if create_student:
-                    student_batch_vals = {
-                        "student_id": create_student.id,
-                        "batch_id": registration.event_id.batch_id.id,
-                        "event_id": registration.event_id.id,
-                    }
-                    create_student_batch = (
-                        self.env["op.batch.students"].sudo().create(student_batch_vals)
-                    )
+                create_student_batch = (
+                    self.env["op.batch.students"].sudo().create(student_batch_vals)
+                )
         return registrations
