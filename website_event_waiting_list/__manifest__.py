c354c8d744c3ca9587b6eba65c8a24842cc6a10b
Miika Nissi <miika@miikanissi.com>
2021-08-26 13:50:13 +0300

    [IMP] improve waiting mail wizard, auditlog rules and registration manage page title

--- website_event_waiting_list/__manifest__.py
+++ website_event_waiting_list/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Website Event Waiting List",
     "summary": "Adds a waiting list functionality to Events.",
-    "version": "14.0.1.2.0",
+    "version": "14.0.1.2.1",
     "category": "Events",
     "website": "https://gitlab.com/tawasta/odoo/event",
     "author": "Tawasta",
@@ -39,5 +39,6 @@
         "views/event_ticket_views.xml",
         "views/event_templates_page_registration.xml",
         "views/event_templates_page_waiting_list.xml",
+        "wizard/waiting_mail_list_message.xml",
     ],
 }
