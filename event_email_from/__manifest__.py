0fa16b5ec51d947ffa0e780c66339c46b8d195c7
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-01-25 09:39:15 +0200

    Modified templates

--- event_email_from/__manifest__.py
+++ event_email_from/__manifest__.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
-#    Author: Oy Tawasta OS Technologies Ltd.
-#    Copyright 2021- Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#    Author: Tawasta
+#    Copyright 2020 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -17,23 +17,18 @@
 #    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
-
 {
-    "name": "Event Mail Attendees",
-    "summary": "Wizard to mail Attendees without mass mail",
-    "version": "14.0.1.0.1",
-    "category": "Events",
+    "name": "Event Email From",
+    "summary": "Event Email From",
+    "version": "14.0.1.0.0",
+    "category": "Website",
     "website": "https://gitlab.com/tawasta/odoo/event",
     "author": "Tawasta",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["event", "mail", "mail_template_name_translatable"],
-    "data": [
-        "data/mail_data.xml",
-        "data/mail_template_data.xml",
-        "wizard/event_mail_attendees.xml",
-        "views/event_views.xml",
-        "security/ir.model.access.csv",
-    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["event", "website_event_waiting_list", "society_event_core"],
+    "data": ["data/email_template_data.xml","views/res_config_settings_view.xml"],
+    "demo": [],
 }
--- event_mail_attendees/models/__init__.py
+++ event_mail_attendees/models/__init__.py
@@ -1,2 +1,1 @@
 from . import event_event
-from . import res_config_settings
