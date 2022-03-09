629589840173caaae5240114a4ba547fca81b665
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-03-09 10:14:19 +0200

    Modified field type and also added possibility to make privacy mandatory

--- website_event_track_privacies/models/privacy.py
+++ website_event_track_privacies/models/privacy.py
@@ -4,8 +4,20 @@ from odoo import models, fields
 class PrivacyActivity(models.Model):
     _inherit = "privacy.activity"
 
+    name = fields.Html(
+        index=True,
+        required=True,
+        translate=True,
+    )
+
     show_in_event = fields.Boolean(
         default=False,
         string="Show in event",
         readonly=False,
+    )
+
+    is_required = fields.Boolean(
+        default=False,
+        string="Is required",
+        readonly=False
     )
