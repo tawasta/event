374c319e7873f95a75b47b61e91d64b655cda31f
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2022-03-09 11:03:13 +0200

    Modified translations

--- website_event_track_privacies/models/event_event.py
+++ website_event_track_privacies/models/event_event.py
@@ -33,27 +33,6 @@ from odoo.exceptions import ValidationError
 # 6. Unknown third party imports:
 
 
-class EventType(models.Model):
-
-    # 1. Private attributes
-    _inherit = "event.type"
-
-    # 2. Fields declaration
-    survey_ids = fields.Many2many("survey.survey", string="Survey")
-
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-
-    # 5. Constraints and onchanges
-
-    # 6. CRUD methods
-
-    # 7. Action methods
-
-    # 8. Business methods
-
-
 class EventEvent(models.Model):
 
     # 1. Private attributes
