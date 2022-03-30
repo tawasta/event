35a4137dcad208ddb66279d9fc900999f87f9f70
Miika Nissi <miika@miikanissi.com>
2022-03-30 19:21:05 +0300

    [IMP] Link event registration to student batch

--- event_batch/models/batch_students.py
+++ event_batch/models/batch_students.py
@@ -15,4 +15,20 @@ class OpBatchStudents(models.Model):
     # 1. Private attributes
     _inherit = "op.batch.students"
 
+    # 2. Fields declaration
     event_id = fields.Many2one(string="Event", comodel_name="event.event")
+    event_registration_id = fields.Many2one(
+        string="Event Registration", comodel_name="event.registration"
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
