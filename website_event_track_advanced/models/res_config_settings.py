66a19a8958a1bf21109bdd5b061891b1d292a74d
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-02-17 09:24:40 +0200

    Added new field and modified template

---
+++ website_event_track_advanced/models/res_config_settings.py
@@ -0,0 +1,11 @@
+from odoo import fields
+from odoo import models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    proposal_see_evaluation = fields.Boolean(
+        string="The submitter of the proposal can see the evaluations",
+        config_parameter="website_event_track_advanced.proposal_see_evaluation",
+    )
