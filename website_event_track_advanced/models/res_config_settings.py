df08c7b032f6c79c8f34e777c486b3eb439af60b
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-11-13 07:06:51 +0200

    Added new field

--- website_event_track_advanced/models/res_config_settings.py
+++ website_event_track_advanced/models/res_config_settings.py
@@ -8,3 +8,8 @@ class ResConfigSettings(models.TransientModel):
         string="The submitter of the proposal can see the evaluations",
         config_parameter="website_event_track_advanced.proposal_see_evaluation",
     )
+
+    evaluator_see_attachments = fields.Boolean(
+        string="Evaluator persons can see the attachments",
+        config_parameter="website_event_track_advanced.evaluator_see_attachments",
+    )
