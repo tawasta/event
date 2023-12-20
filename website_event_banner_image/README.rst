3db75dfc884289623ea50ccd21fe44995a194a94
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-12-20 10:56:37 +0200

    Modified module to 17 version

---
+++ website_event_banner_image/README.rst
@@ -0,0 +1,38 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========================
+Website Event Banner Image
+==========================
+* Ability to add a banner image from backend to an event or event type.
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+- Each image has to be uploaded from users computer (similar to product images).
+- No optimization for handling multiples of the same image. Each image is uploaded and handled as a new image.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Miika Nissi <miika.nissi@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
--- website_event_private_event/controllers/main.py
+++ website_event_private_event/controllers/main.py
@@ -23,7 +23,7 @@
 # 2. Known third party imports:
 
 from collections import defaultdict
-
+import logging
 from dateutil.relativedelta import relativedelta
 
 # 3. Odoo imports (openerp):
@@ -45,21 +45,128 @@ from odoo.addons.website_event.controllers.main import WebsiteEventController
 
 
 class WebsiteEventControllerPrivateEvent(WebsiteEventController):
-    @http.route()
+    def sitemap_event(env, rule, qs):
+        if not qs or qs.lower() in '/events':
+            yield {'loc': '/events'}
+
+    # ------------------------------------------------------------
+    # EVENT LIST
+    # ------------------------------------------------------------
+
+    def _get_events_search_options(self, **post):
+        return {
+            'displayDescription': False,
+            'displayDetail': False,
+            'displayExtraDetail': False,
+            'displayExtraLink': False,
+            'displayImage': False,
+            'is_private_event': False,
+            'allowFuzzy': not post.get('noFuzzy'),
+            'date': post.get('date'),
+            'tags': post.get('tags'),
+            'type': post.get('type'),
+            'country': post.get('country'),
+        }
+
+    @http.route(['/event', '/event/page/<int:page>', '/events', '/events/page/<int:page>'], type='http', auth="public", website=True, sitemap=sitemap_event)
     def events(self, page=1, **searches):
-        # Kutsu alkuperäistä metodia
-        response = super(WebsiteEventControllerPrivateEvent, self).events(page, **searches)
+        Event = request.env['event.event']
+        SudoEventType = request.env['event.type'].sudo()
 
-        # Ota event_ids arvot responsesta
-        events = response.qcontext.get('event_ids', [])
+        searches.setdefault('search', '')
+        searches.setdefault('date', 'upcoming')
+        searches.setdefault('tags', '')
+        searches.setdefault('type', 'all')
+        searches.setdefault('country', 'all')
 
-        # Suodata pois yksityiset tapahtumat
-        filtered_events = [event for event in events if not event.is_private_event]
+        website = request.website
 
-        # Päivitä suodatetut tapahtumat vastaukseen
-        response.qcontext['event_ids'] = filtered_events
+        step = 12  # Number of events per page
 
-        return response
+        options = self._get_events_search_options(**searches)
+        logging.info(options);
+        order = 'date_begin'
+        if searches.get('date', 'upcoming') == 'old':
+            order = 'date_begin desc'
+        order = 'is_published desc, ' + order
+        search = searches.get('search')
+        logging.info(search);
+        event_count, details, fuzzy_search_term = website._search_with_fuzzy("events", search,
+            limit=page * step, order=order, options=options)
+        logging.info(event_count);
+        logging.info(details);
+        event_details = details[0]
+        events = event_details.get('results', Event)
+        events = events[(page - 1) * step:page * step]
+
+        # count by domains without self search
+        domain_search = [('name', 'ilike', fuzzy_search_term or searches['search'])] if searches['search'] else []
+
+        no_date_domain = event_details['no_date_domain']
+        dates = event_details['dates']
+        for date in dates:
+            if date[0] not in ['all', 'old']:
+                date[3] = Event.search_count(expression.AND(no_date_domain) + domain_search + date[2])
+
+        no_country_domain = event_details['no_country_domain']
+        countries = Event.read_group(expression.AND(no_country_domain) + domain_search, ["id", "country_id"],
+            groupby="country_id", orderby="country_id")
+        countries.insert(0, {
+            'country_id_count': sum([int(country['country_id_count']) for country in countries]),
+            'country_id': ("all", _("All Countries"))
+        })
+
+        search_tags = event_details['search_tags']
+        current_date = event_details['current_date']
+        current_type = None
+        current_country = None
+
+        if searches["type"] != 'all':
+            current_type = SudoEventType.browse(int(searches['type']))
+
+        if searches["country"] != 'all' and searches["country"] != 'online':
+            current_country = request.env['res.country'].browse(int(searches['country']))
+
+        pager = website.pager(
+            url="/event",
+            url_args=searches,
+            total=event_count,
+            page=page,
+            step=step,
+            scope=5)
+
+        keep = QueryURL('/event', **{
+            key: value for key, value in searches.items() if (
+                key == 'search' or
+                (value != 'upcoming' if key == 'date' else value != 'all'))
+            })
+
+        searches['search'] = fuzzy_search_term or search
+
+        values = {
+            'current_date': current_date,
+            'current_country': current_country,
+            'current_type': current_type,
+            'event_ids': events,  # event_ids used in website_event_track so we keep name as it is
+            'dates': dates,
+            'categories': request.env['event.tag.category'].search([
+                ('is_published', '=', True), '|', ('website_id', '=', website.id), ('website_id', '=', False)
+            ]),
+            'countries': countries,
+            'pager': pager,
+            'searches': searches,
+            'search_tags': search_tags,
+            'keep': keep,
+            'search_count': event_count,
+            'original_search': fuzzy_search_term and search,
+            'website': website
+        }
+
+        if searches['date'] == 'old':
+            # the only way to display this content is to set date=old so it must be canonical
+            values['canonical_params'] = OrderedMultiDict([('date', 'old')])
+
+        return request.render("website_event.index", values)
 
     @http.route(
         "/event/get_country_event_list", type="json", auth="public", website=True
