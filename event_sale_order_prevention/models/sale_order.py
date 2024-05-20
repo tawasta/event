fc1f119e26670e7f83951c7170d8dc379193793e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-05-20 13:21:07 +0300

    [ADD] - created new module

---
+++ event_sale_order_prevention/models/sale_order.py
@@ -0,0 +1,24 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def unlink(self):
+        if self.env['event.registration'].search_count([('sale_order_id', 'in', self.ids)]) > 0:
+            raise UserError(_("Cannot delete Sale Orders with associated event registrations."))
+        else:
+            return super(SaleOrder, self).unlink()
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    def unlink(self):
+        registration_count = self.env['event.registration'].search_count([('sale_order_line_id', 'in', self.ids)])
+        if registration_count > 0:
+            raise UserError(_("Cannot delete Sale Order Lines with associated event registrations."))
+        else:
+            return super(SaleOrderLine, self).unlink()
+
