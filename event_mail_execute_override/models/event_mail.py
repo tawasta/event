b721ed8783d9d812f33408773ff430c0f813278a
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-05-24 14:17:13 +0300

    [IMP] - added more logs

--- event_mail_execute_override/models/event_mail.py
+++ event_mail_execute_override/models/event_mail.py
@@ -2,9 +2,9 @@ from odoo import fields, models
 from datetime import datetime, timedelta
 import logging
 
+_logger = logging.getLogger(__name__)
 
 class EventMailScheduler(models.Model):
-    # 1. Private attributes
     _inherit = "event.mail"
 
     def update_feedback_survey(self, mail):
@@ -18,11 +18,9 @@ class EventMailScheduler(models.Model):
                 )
 
     def process_registrations_based_on_interval(self, mail):
-        # Apufunktio, joka käsittelee rekisteröintirivit interval_type perusteella
         lines = []
         is_mail_valid = False
 
-        # Suodatetaan rekisteröinnit mail.interval_type perusteella
         if mail.interval_type == "after_sub":
             lines = [
                 (0, 0, {"registration_id": registration.id})
@@ -37,7 +35,6 @@ class EventMailScheduler(models.Model):
 
     def check_and_send_mail(self, mail):
         now = fields.Datetime.now()
-        # Tarkista, voidaanko sähköposti lähettää
         if (
             not mail.mail_sent
             and mail.scheduled_date <= now
@@ -47,14 +44,13 @@ class EventMailScheduler(models.Model):
             return True
         return False
 
-    # flake8: noqa: C901
     def execute(self):
         now = fields.Datetime.now()
         delay_time = timedelta(minutes=1)
         start_time = datetime.now() + delay_time
         for mail in self:
+            _logger.info("===EXECUTE ALOITUS===")
 
-            # Hae aktiivisen sivuston rajoitetut sähköpostipohjat
             Website = self.env["website"].sudo()
             current_website = Website.get_current_website()
             restricted_templates = current_website.restricted_mail_template_ids.ids
@@ -62,21 +58,15 @@ class EventMailScheduler(models.Model):
 
             if mail.event_id.date_end < now:
                 if not is_restricted_template:
-
-                    # Kutsutaan apufunktiota ja saadaan sekä käsitellyt rivit että mailin tila
-                    lines, is_mail_valid = self.process_registrations_based_on_interval(
-                        mail
-                    )
-                    # Jos mail on validi ja on käsiteltyjä rivejä, päivitä mail_registration_ids
+                    lines, is_mail_valid = self.process_registrations_based_on_interval(mail)
                     if is_mail_valid:
                         mail.write({"mail_registration_ids": lines})
                         self.update_feedback_survey(mail)
-                        logging.info("===LAHETETAAN EXECUTE====");
+                        _logger.info("===LAHETETAAN EXECUTE 1====")
                         mail.mail_registration_ids.with_delay(eta=start_time).execute()
                     else:
                         mail_was_sent = self.check_and_send_mail(mail)
                         if mail_was_sent:
-
                             if mail.template_id.is_feedback_email:
                                 registrations = mail.event_id.registration_ids.filtered(
                                     lambda r: r.state != "cancel"
@@ -96,26 +86,20 @@ class EventMailScheduler(models.Model):
                                             "feedback_survey_id": mail.event_id.feedback_survey_id.id
                                         }
                                     )
-
+                            _logger.info("===LAHETETAAN MAIL ATTENDEES 1===")
                             mail.event_id.with_delay(eta=start_time).mail_attendees(mail.template_id.id)
                             mail.write({"mail_sent": True})
 
             else:
-
-                # Kutsutaan apufunktiota ja saadaan sekä käsitellyt rivit että mailin tila
-                lines, is_mail_valid = self.process_registrations_based_on_interval(
-                    mail
-                )
-                # Jos mail on validi ja on käsiteltyjä rivejä, päivitä mail_registration_ids
+                lines, is_mail_valid = self.process_registrations_based_on_interval(mail)
                 if is_mail_valid:
                     mail.write({"mail_registration_ids": lines})
                     self.update_feedback_survey(mail)
-                    logging.info("===LAHETETAAN EXECUTE 2====");
+                    _logger.info("===LAHETETAAN EXECUTE 2====")
                     mail.mail_registration_ids.with_delay(eta=start_time).execute()
                 else:
                     mail_was_sent = self.check_and_send_mail(mail)
                     if mail_was_sent:
-
                         if mail.template_id.is_feedback_email:
                             registrations = mail.event_id.registration_ids.filtered(
                                 lambda r: r.state != "cancel"
@@ -133,7 +117,8 @@ class EventMailScheduler(models.Model):
                                         "feedback_survey_id": mail.event_id.feedback_survey_id.id
                                     }
                                 )
-
+                        _logger.info("===LAHETETAAN MAIL ATTENDEES 2===")
                         mail.event_id.with_delay(eta=start_time).mail_attendees(mail.template_id.id)
                         mail.write({"mail_sent": True})
+            _logger.info("===EXECUTE LOPPUN===")
         return True
