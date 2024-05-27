ee8efa2b83ffdfbf7be1fa4a2620515644265ba7
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-05-27 09:47:20 +0300

    [IMP] - removed logs

--- event_mail_execute_override/models/event_mail.py
+++ event_mail_execute_override/models/event_mail.py
@@ -1,11 +1,9 @@
-from odoo import fields, models, api, SUPERUSER_ID
+from odoo import fields, models
 from datetime import datetime, timedelta
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class EventMailScheduler(models.Model):
+    # 1. Private attributes
     _inherit = "event.mail"
 
     def update_feedback_survey(self, mail):
@@ -19,9 +17,11 @@ class EventMailScheduler(models.Model):
                 )
 
     def process_registrations_based_on_interval(self, mail):
+        # Apufunktio, joka käsittelee rekisteröintirivit interval_type perusteella
         lines = []
         is_mail_valid = False
 
+        # Suodatetaan rekisteröinnit mail.interval_type perusteella
         if mail.interval_type == "after_sub":
             lines = [
                 (0, 0, {"registration_id": registration.id})
@@ -36,6 +36,7 @@ class EventMailScheduler(models.Model):
 
     def check_and_send_mail(self, mail):
         now = fields.Datetime.now()
+        # Tarkista, voidaanko sähköposti lähettää
         if (
             not mail.mail_sent
             and mail.scheduled_date <= now
@@ -45,18 +46,14 @@ class EventMailScheduler(models.Model):
             return True
         return False
 
+    # flake8: noqa: C901
     def execute(self):
         now = fields.Datetime.now()
         delay_time = timedelta(minutes=1)
         start_time = datetime.now() + delay_time
-        processed_mail_ids = set()  # Set to track processed mail IDs
         for mail in self:
-            if mail.id in processed_mail_ids:
-                _logger.info(f"Skipping already processed mail with ID {mail.id}")
-                continue
 
-            _logger.info(f"===EXECUTE ALOITUS mail_id: {mail.id}===")
-
+            # Hae aktiivisen sivuston rajoitetut sähköpostipohjat
             Website = self.env["website"].sudo()
             current_website = Website.get_current_website()
             restricted_templates = current_website.restricted_mail_template_ids.ids
@@ -64,46 +61,77 @@ class EventMailScheduler(models.Model):
 
             if mail.event_id.date_end < now:
                 if not is_restricted_template:
-                    lines, is_mail_valid = self.process_registrations_based_on_interval(mail)
+
+                    # Kutsutaan apufunktiota ja saadaan sekä käsitellyt rivit että mailin tila
+                    lines, is_mail_valid = self.process_registrations_based_on_interval(
+                        mail
+                    )
+                    # Jos mail on validi ja on käsiteltyjä rivejä, päivitä mail_registration_ids
                     if is_mail_valid:
                         mail.write({"mail_registration_ids": lines})
                         self.update_feedback_survey(mail)
-                        _logger.info(f"===LAHETETAAN EXECUTE 1 mail_id: {mail.id}===")
+                        
                         mail.mail_registration_ids.execute()
                     else:
                         mail_was_sent = self.check_and_send_mail(mail)
                         if mail_was_sent:
+
                             if mail.template_id.is_feedback_email:
                                 registrations = mail.event_id.registration_ids.filtered(
                                     lambda r: r.state != "cancel"
                                 )
                                 if mail.feedback_survey_id:
-                                    registrations.write({"feedback_survey_id": mail.feedback_survey_id.id})
-                                elif (mail.event_id.feedback_survey_id and not mail.feedback_survey_id):
-                                    registrations.write({"feedback_survey_id": mail.event_id.feedback_survey_id.id})
-                            _logger.info(f"===LAHETETAAN MAIL ATTENDEES 1 mail_id: {mail.id}===")
+                                    registrations.write(
+                                        {
+                                            "feedback_survey_id": mail.feedback_survey_id.id
+                                        }
+                                    )
+                                elif (
+                                    mail.event_id.feedback_survey_id
+                                    and not mail.feedback_survey_id
+                                ):
+                                    registrations.write(
+                                        {
+                                            "feedback_survey_id": mail.event_id.feedback_survey_id.id
+                                        }
+                                    )
+
                             mail.event_id.mail_attendees(mail.template_id.id)
                             mail.write({"mail_sent": True})
+
             else:
-                lines, is_mail_valid = self.process_registrations_based_on_interval(mail)
+
+                # Kutsutaan apufunktiota ja saadaan sekä käsitellyt rivit että mailin tila
+                lines, is_mail_valid = self.process_registrations_based_on_interval(
+                    mail
+                )
+                # Jos mail on validi ja on käsiteltyjä rivejä, päivitä mail_registration_ids
                 if is_mail_valid:
                     mail.write({"mail_registration_ids": lines})
                     self.update_feedback_survey(mail)
-                    _logger.info(f"===LAHETETAAN EXECUTE 2 mail_id: {mail.id}===")
                     mail.mail_registration_ids.execute()
                 else:
                     mail_was_sent = self.check_and_send_mail(mail)
                     if mail_was_sent:
+
                         if mail.template_id.is_feedback_email:
-                            registrations = mail.event_id.registration_ids.filtered(lambda r: r.state != "cancel")
+                            registrations = mail.event_id.registration_ids.filtered(
+                                lambda r: r.state != "cancel"
+                            )
                             if mail.feedback_survey_id:
-                                registrations.write({"feedback_survey_id": mail.feedback_survey_id.id})
-                            elif (mail.event_id.feedback_survey_id and not mail.feedback_survey_id):
-                                registrations.write({"feedback_survey_id": mail.event_id.feedback_survey_id.id})
-                        _logger.info(f"===LAHETETAAN MAIL ATTENDEES 2 mail_id: {mail.id}===")
+                                registrations.write(
+                                    {"feedback_survey_id": mail.feedback_survey_id.id}
+                                )
+                            elif (
+                                mail.event_id.feedback_survey_id
+                                and not mail.feedback_survey_id
+                            ):
+                                registrations.write(
+                                    {
+                                        "feedback_survey_id": mail.event_id.feedback_survey_id.id
+                                    }
+                                )
+
                         mail.event_id.mail_attendees(mail.template_id.id)
                         mail.write({"mail_sent": True})
-
-            processed_mail_ids.add(mail.id)
-            _logger.info(f"===EXECUTE LOPPUN mail_id: {mail.id}===")
         return True
