0f1975f34ad24e433696f4b5287e908454f2d93b
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-07-17 11:27:59 +0300

    [IMP] - modified module to 17 version

---
+++ event_mail_attendees/models/event_event.py
@@ -0,0 +1,69 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2021- Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class EventEvent(models.Model):
+    # 1. Private attributes
+    _inherit = "event.event"
+
+    # 2. Fields declaration
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+    def action_message_attendees(self):
+        self.ensure_one()
+        template = self.env.ref(
+            "event_mail_attendees.event_mail_template_mail_attendees",
+            raise_if_not_found=False,
+        )
+        local_context = dict(
+            self.env.context,
+            default_event_id=self.id,
+            default_template_id=template and template.id or False,
+        )
+        return {
+            "type": "ir.actions.act_window",
+            "view_mode": "form",
+            "res_model": "event.mail.attendees.wizard",
+            "target": "new",
+            "context": local_context,
+        }
+
+    # 8. Business methods
