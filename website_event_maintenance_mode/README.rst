a86d2273bc4c8935a447cd8c42b6ae11bc8d4d51
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-09-25 13:05:39 +0300

    Add maintenance mode to website events

---
+++ website_event_maintenance_mode/README.rst
@@ -0,0 +1,45 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=============================
+Website Sale Maintenance Mode
+=============================
+
+* Enables putting the shop in maintenance mode, in which the visitor
+  is shown a message instead of the actual pages.
+
+Installation
+============
+\-
+
+Configuration
+=============
+* Go to Website settings, check the "Website sale: Maintenance mode" box
+  and type in a text shown for the visiting users.
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Valtteri Lattu <valtteri.lattu@tawasta.fi>
+* Timo Talvitie <timo.talvitie@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: https://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: https://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ website_event_maintenance_mode/controllers/main.py
@@ -0,0 +1,62 @@
+from odoo import http
+from odoo.http import request
+
+from odoo.addons.website_event.controllers.main import WebsiteEventController
+
+
+class WebsiteEventControllerMaintenance(WebsiteEventController):
+    @http.route()
+    def events(self, page=1, **searches):
+        render_values = {"company": request.website.company_id}
+        if request.website._maintenance_mode():
+            return request.render(
+                "website_sale_maintenance_mode.website_sale_template", render_values
+            )
+        else:
+            response = super().events(
+                page=page, **searches
+            )
+
+            return response
+
+    @http.route()
+    def event_page(self, event, page, **post):
+        render_values = {"company": request.website.company_id}
+        if request.website._maintenance_mode():
+            return request.render(
+                "website_sale_maintenance_mode.website_sale_template", render_values
+            )
+        else:
+            response = super().event_page(
+                event=event, page=page, **post
+            )
+
+            return response
+
+    @http.route()
+    def event(self, event, **post):
+        render_values = {"company": request.website.company_id}
+        if request.website._maintenance_mode():
+            return request.render(
+                "website_sale_maintenance_mode.website_sale_template", render_values
+            )
+        else:
+            response = super().event(
+                event=event, **post
+            )
+
+            return response
+
+    @http.route()
+    def event_register(self, event, **post):
+        render_values = {"company": request.website.company_id}
+        if request.website._maintenance_mode():
+            return request.render(
+                "website_sale_maintenance_mode.website_sale_template", render_values
+            )
+        else:
+            response = super().event_register(
+                event=event, **post
+            )
+
+            return response
