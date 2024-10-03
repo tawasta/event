829a346549ec222b36440896f7fbc2cb333464e7
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-10-03 09:26:04 +0300

    [IMP] - modified function

--- website_event_private_event/controllers/main.py
+++ website_event_private_event/controllers/main.py
@@ -22,7 +22,6 @@
 
 # 2. Known third party imports:
 
-import logging
 
 # 3. Odoo imports (openerp):
 from werkzeug.datastructures import OrderedMultiDict
@@ -52,28 +51,34 @@ class WebsiteEventControllerPrivateEvent(WebsiteEventController):
 
     def _get_events_search_options(self, **post):
         return {
-            'displayDescription': False,
-            'displayDetail': False,
-            'displayExtraDetail': False,
-            'displayExtraLink': False,
-            'displayImage': False,
-            'allowFuzzy': not post.get('noFuzzy'),
-            'date': post.get('date'),
-            'tags': post.get('tags'),
-            'type': post.get('type'),
-            'country': post.get('country'),
+            "displayDescription": False,
+            "displayDetail": False,
+            "displayExtraDetail": False,
+            "displayExtraLink": False,
+            "displayImage": False,
+            "allowFuzzy": not post.get("noFuzzy"),
+            "date": post.get("date"),
+            "tags": post.get("tags"),
+            "type": post.get("type"),
+            "country": post.get("country"),
         }
 
-    @http.route(['/event', '/event/page/<int:page>', '/events', '/events/page/<int:page>'], type='http', auth="public", website=True, sitemap=sitemap_event)
+    @http.route(
+        ["/event", "/event/page/<int:page>", "/events", "/events/page/<int:page>"],
+        type="http",
+        auth="public",
+        website=True,
+        sitemap=sitemap_event,
+    )
     def events(self, page=1, **searches):
-        Event = request.env['event.event']
-        SudoEventType = request.env['event.type'].sudo()
+        Event = request.env["event.event"]
+        SudoEventType = request.env["event.type"].sudo()
 
-        searches.setdefault('search', '')
-        searches.setdefault('date', 'upcoming')
-        searches.setdefault('tags', '')
-        searches.setdefault('type', 'all')
-        searches.setdefault('country', 'all')
+        searches.setdefault("search", "")
+        searches.setdefault("date", "upcoming")
+        searches.setdefault("tags", "")
+        searches.setdefault("type", "all")
+        searches.setdefault("country", "all")
 
         website = request.website
 
@@ -81,47 +86,65 @@ class WebsiteEventControllerPrivateEvent(WebsiteEventController):
 
         options = self._get_events_search_options(**searches)
 
-        order = 'date_begin'
-        if searches.get('date', 'upcoming') == 'old':
-            order = 'date_begin desc'
-        order = 'is_published desc, ' + order
-        search = searches.get('search')
+        order = "date_begin"
+        if searches.get("date", "upcoming") == "old":
+            order = "date_begin desc"
+        order = "is_published desc, " + order
+        search = searches.get("search")
 
-        event_count, details, fuzzy_search_term = website._search_with_fuzzy("events", search,
-            limit=page * step, order=order, options=options)
+        event_count, details, fuzzy_search_term = website._search_with_fuzzy(
+            "events", search, limit=page * step, order=order, options=options
+        )
         event_details = details[0]
 
-        events = event_details.get('results', Event)
+        events = event_details.get("results", Event)
         events = [event for event in events if not event.is_private_event]
-        events = events[(page - 1) * step:page * step]
+        events = events[(page - 1) * step : page * step]
 
         # count by domains without self search
-        domain_search = [('name', 'ilike', fuzzy_search_term or searches['search'])] if searches['search'] else []
+        domain_search = (
+            [("name", "ilike", fuzzy_search_term or searches["search"])]
+            if searches["search"]
+            else []
+        )
 
-        no_date_domain = event_details['no_date_domain']
-        dates = event_details['dates']
+        no_date_domain = event_details["no_date_domain"]
+        dates = event_details["dates"]
         for date in dates:
-            if date[0] not in ['all', 'old']:
-                date[3] = Event.search_count(expression.AND(no_date_domain) + domain_search + date[2])
+            if date[0] not in ["all", "old"]:
+                date[3] = Event.search_count(
+                    expression.AND(no_date_domain) + domain_search + date[2]
+                )
 
-        no_country_domain = event_details['no_country_domain']
-        countries = Event.read_group(expression.AND(no_country_domain) + domain_search, ["id", "country_id"],
-            groupby="country_id", orderby="country_id")
-        countries.insert(0, {
-            'country_id_count': sum([int(country['country_id_count']) for country in countries]),
-            'country_id': ("all", _("All Countries"))
-        })
+        no_country_domain = event_details["no_country_domain"]
+        countries = Event.read_group(
+            expression.AND(no_country_domain) + domain_search,
+            ["id", "country_id"],
+            groupby="country_id",
+            orderby="country_id",
+        )
+        countries.insert(
+            0,
+            {
+                "country_id_count": sum(
+                    int(country["country_id_count"]) for country in countries
+                ),
+                "country_id": ("all", _("All Countries")),
+            },
+        )
 
-        search_tags = event_details['search_tags']
-        current_date = event_details['current_date']
+        search_tags = event_details["search_tags"]
+        current_date = event_details["current_date"]
         current_type = None
         current_country = None
 
-        if searches["type"] != 'all':
-            current_type = SudoEventType.browse(int(searches['type']))
+        if searches["type"] != "all":
+            current_type = SudoEventType.browse(int(searches["type"]))
 
-        if searches["country"] != 'all' and searches["country"] != 'online':
-            current_country = request.env['res.country'].browse(int(searches['country']))
+        if searches["country"] != "all" and searches["country"] != "online":
+            current_country = request.env["res.country"].browse(
+                int(searches["country"])
+            )
 
         pager = website.pager(
             url="/event",
@@ -129,38 +152,50 @@ class WebsiteEventControllerPrivateEvent(WebsiteEventController):
             total=event_count,
             page=page,
             step=step,
-            scope=5)
+            scope=5,
+        )
 
-        keep = QueryURL('/event', **{
-            key: value for key, value in searches.items() if (
-                key == 'search' or
-                (value != 'upcoming' if key == 'date' else value != 'all'))
-            })
+        keep = QueryURL(
+            "/event",
+            **{
+                key: value
+                for key, value in searches.items()
+                if (
+                    key == "search"
+                    or (value != "upcoming" if key == "date" else value != "all")
+                )
+            }
+        )
 
-        searches['search'] = fuzzy_search_term or search
+        searches["search"] = fuzzy_search_term or search
 
         values = {
-            'current_date': current_date,
-            'current_country': current_country,
-            'current_type': current_type,
-            'event_ids': events,  # event_ids used in website_event_track so we keep name as it is
-            'dates': dates,
-            'categories': request.env['event.tag.category'].search([
-                ('is_published', '=', True), '|', ('website_id', '=', website.id), ('website_id', '=', False)
-            ]),
-            'countries': countries,
-            'pager': pager,
-            'searches': searches,
-            'search_tags': search_tags,
-            'keep': keep,
-            'search_count': event_count,
-            'original_search': fuzzy_search_term and search,
-            'website': website
+            "current_date": current_date,
+            "current_country": current_country,
+            "current_type": current_type,
+            "event_ids": events,
+            "dates": dates,
+            "categories": request.env["event.tag.category"].search(
+                [
+                    ("is_published", "=", True),
+                    "|",
+                    ("website_id", "=", website.id),
+                    ("website_id", "=", False),
+                ]
+            ),
+            "countries": countries,
+            "pager": pager,
+            "searches": searches,
+            "search_tags": search_tags,
+            "keep": keep,
+            "search_count": event_count,
+            "original_search": fuzzy_search_term and search,
+            "website": website,
         }
 
-        if searches['date'] == 'old':
+        if searches["date"] == "old":
             # the only way to display this content is to set date=old so it must be canonical
-            values['canonical_params'] = OrderedMultiDict([('date', 'old')])
+            values["canonical_params"] = OrderedMultiDict([("date", "old")])
 
         return request.render("website_event.index", values)
 
