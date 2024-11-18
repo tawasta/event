d540fe095039f4057c931d9aac7300302b651a8e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2024-11-18 13:01:20 +0200

    [IMP] - added more security checks

--- event_ticket_purchase_options/controllers/main.py
+++ event_ticket_purchase_options/controllers/main.py
@@ -247,6 +247,9 @@ class EventRegistrationController(WebsiteEventController):
         if not event:
             return request.render("website.403")
 
+        if request.env.user.partner_id.email != invitation.invite_email:
+            return request.render("website.403")
+
         if invitation.is_used:
             values = {
                 "invitation": invitation,
