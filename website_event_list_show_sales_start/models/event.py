952c81646bae17d90663610a0e081a3d4541d94e
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-04-09 16:27:04 +0300

    [IMP] access rights check for publishing events that are <30 days away; run precommit for whole repo

--- website_event_list_show_sales_start/models/event.py
+++ website_event_list_show_sales_start/models/event.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields
+from odoo import fields, models
 
 
 class Event(models.Model):
@@ -6,13 +6,15 @@ class Event(models.Model):
     _inherit = "event.event"
 
     event_ticket_earliest_start_sale = fields.Datetime(
-        string='Earliest Ticket Start of Sale',
-        compute='_compute_event_ticket_earliest_start_sale'
+        string="Earliest Ticket Start of Sale",
+        compute="_compute_event_ticket_earliest_start_sale",
     )
 
     def _compute_event_ticket_earliest_start_sale(self):
         for event in self:
             event.event_ticket_earliest_start_sale = min(
-                event.event_ticket_ids.filtered(lambda ticket: ticket.start_sale_datetime).mapped('start_sale_datetime'),
-                default=False
+                event.event_ticket_ids.filtered(
+                    lambda ticket: ticket.start_sale_datetime
+                ).mapped("start_sale_datetime"),
+                default=False,
             )
