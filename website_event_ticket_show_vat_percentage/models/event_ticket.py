2fbb54ffa7b4c30cea6ca82832b25b72c5e8faaf
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-08-27 08:18:30 +0300

    Add translation

--- website_event_ticket_show_vat_percentage/models/event_ticket.py
+++ website_event_ticket_show_vat_percentage/models/event_ticket.py
@@ -1,16 +1,16 @@
 from odoo import models
-import sys
-import pprint
+from odoo import _
 
 class EventEventTicket(models.Model):
     _inherit = "event.event.ticket"
 
     def _get_register_modal_vat_text(self):
         ticket = self
+        including_vat = _("inc. VAT")
         website = self.env["website"].get_current_website(fallback=False)
         if website.show_line_subtotals_tax_selection == "tax_excluded":
             return ""
         elif not ticket.product_id.taxes_id.display_name:
             return ""
         else:
-            return "sis. alv. " + ticket.product_id.taxes_id.display_name
+            return including_vat + " " + ticket.product_id.taxes_id.display_name
