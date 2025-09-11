23b8b9be32a1a6451b1c6b30781dea7cd6a88de9
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-09-11 08:12:46 +0300

    [IMP] - modified function

--- website_event_track_advanced/controllers/event_track.py
+++ website_event_track_advanced/controllers/event_track.py
@@ -888,11 +888,12 @@ class EventTrackControllerAdvanced(EventTrackController):
             _logger.warning(_("Could not create organization (missing name)"))
             return False
 
-        organization = (
+        """organization = (
             request.env["res.partner"]
             .sudo()
             .search([("name", "=ilike", organization_name)], limit=1)
-        )
+        )"""
+        organization = False
 
         if not organization:
             organization_values["is_company"] = True
