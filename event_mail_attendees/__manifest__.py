5099de7298b7c7a92b495d5bbd6a80cffcfe6144
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-23 08:08:24 +0200

    [MIG] 19.0 ecent_mail_attendees

---
+++ event_mail_attendees/__manifest__.py
@@ -0,0 +1,39 @@
+##############################################################################
+#
+#    Author: Futural Oy
+#    Copyright 2021- Futural Oy (https://futural.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Event Mail Attendees",
+    "summary": "Wizard to mail Attendees without mass mail",
+    "version": "19.0.1.0.0",
+    "category": "Events",
+    "website": "https://github.com/tawasta/event",
+    "author": "Futural",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["event", "mail", "mail_template_name_translatable"],
+    "data": [
+        "data/mail_data.xml",
+        "data/mail_template_data.xml",
+        "wizard/event_mail_attendees.xml",
+        "views/event_views.xml",
+        "security/ir.model.access.csv",
+    ],
+}
