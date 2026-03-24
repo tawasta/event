0ab9e2251ddce2fccae98349877190901399ba05
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-24 08:32:14 +0200

    Fix publishing on correct state change

--- website_event_publish_event/models/event_event.py
+++ website_event_publish_event/models/event_event.py
@@ -1,24 +1,8 @@
-##############################################################################
-#
-#    Author: Futural Oy
-#    Copyright 2022- Futural Oy (https://futural.fi)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
-#
-##############################################################################
+import logging
 
 from odoo import models
+
+_logger = logging.getLogger(__name__)
 
 
 class EventEvent(models.Model):
@@ -26,16 +10,20 @@ class EventEvent(models.Model):
 
     def write(self, vals):
         res = super().write(vals)
-        if "website_published" in vals and vals.get("website_published"):
-            self.action_set_stage_published()
+        self.action_set_stage_published(vals.get("stage_id"))
         return res
 
     def action_publish_event(self):
         self.website_published = True
 
-    def action_set_stage_published(self):
-        first_published_stage = self.env["event.stage"].search(
-            [("pipe_publish", "=", True)], order="sequence"
-        )
-        if first_published_stage:
-            self.write({"stage_id": first_published_stage[0].id})
+    def action_set_stage_published(self, stage_id):
+        if (
+            stage_id
+            and len(
+                self.env["event.stage"].search(
+                    [("id", "=", stage_id), ("pipe_publish", "=", True)]
+                )
+            )
+            > 0
+        ):
+            self.action_publish_event()
