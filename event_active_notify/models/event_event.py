37a46ed8e55994a9879806902a7867127e4d1938
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-03-31 08:58:11 +0300

    [IMP] - added logs

--- event_active_notify/models/event_event.py
+++ event_active_notify/models/event_event.py
@@ -16,53 +16,20 @@ class EventEvent(models.Model):
     def create(self, vals):
         _logger.info("event_active_notify: create called with vals=%s", vals)
         event = super().create(vals)
-        _logger.info(
-            "event_active_notify: created event id=%s name=%s is_published=%s notification_sent=%s",
-            event.id,
-            event.name,
-            event.is_published,
-            event.notification_sent,
-        )
+
         if vals.get("is_published") and not event.notification_sent:
-            _logger.info(
-                "event_active_notify: triggering _notify_group_users from create for event id=%s",
-                event.id,
-            )
             event._notify_group_users()
         return event
 
     def write(self, vals):
-        _logger.info(
-            "event_active_notify: write called for event ids=%s with vals=%s",
-            self.ids,
-            vals,
-        )
         res = super().write(vals)
         if vals.get("is_published"):
-            _logger.info(
-                "event_active_notify: is_published found in write vals for event ids=%s",
-                self.ids,
-            )
             for event in self:
-                _logger.info(
-                    "event_active_notify: checking event id=%s name=%s notification_sent=%s",
-                    event.id,
-                    event.name,
-                    event.notification_sent,
-                )
                 if not event.notification_sent:
-                    _logger.info(
-                        "event_active_notify: triggering _notify_group_users from write for event id=%s",
-                        event.id,
-                    )
                     event._notify_group_users()
         return res
 
     def _notify_group_users(self):
-        _logger.info(
-            "event_active_notify: _notify_group_users called for event ids=%s",
-            self.ids,
-        )
         group = self.env.ref(
             "event_active_notify.group_event_notifications",
             raise_if_not_found=False,  # noqa: E501
@@ -83,18 +50,7 @@ class EventEvent(models.Model):
                 recipients.ids,
             )
             for event in self:
-                _logger.info(
-                    "event_active_notify: sending activation notifications for event id=%s name=%s",
-                    event.id,
-                    event.name,
-                )
                 for recipient in recipients:
-                    _logger.info(
-                        "event_active_notify: sending activation mail for event id=%s to user_id=%s email=%s",
-                        event.id,
-                        recipient.id,
-                        recipient.partner_id.email,
-                    )
                     template.with_context(
                         lang=recipient.lang or self.env.user.lang,
                     ).send_mail(
@@ -172,18 +128,7 @@ class EventEvent(models.Model):
         )
 
         for event in events:
-            _logger.info(
-                "event_active_notify: cron sending ticket sale notifications for event id=%s name=%s",
-                event.id,
-                event.name,
-            )
             for recipient in recipients:
-                _logger.info(
-                    "event_active_notify: cron sending ticket sale mail for event id=%s to user_id=%s email=%s",
-                    event.id,
-                    recipient.id,
-                    recipient.partner_id.email,
-                )
                 template.with_context(
                     lang=recipient.lang or self.env.user.lang,
                 ).send_mail(
@@ -192,7 +137,3 @@ class EventEvent(models.Model):
                     email_values={"email_to": recipient.partner_id.email},
                 )
             event.ticket_sale_notification_sent = True
-            _logger.info(
-                "event_active_notify: marked ticket_sale_notification_sent=True for event id=%s",
-                event.id,
-            )
