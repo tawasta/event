e7718b7f973b0b5c769021e5405aae067f8df0d2
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-04-20 11:42:21 +0300

    [IMP] - modified function

--- event_registration_limit/controllers/event.py
+++ event_registration_limit/controllers/event.py
@@ -6,44 +6,56 @@ from odoo.http import request
 class CustomWebsiteEventRegistrationController(WebsiteEventController):
     @http.route()
     def event_register(self, event, **post):
-        partner_id = (
-            request.env.user.partner_id.id if request.env.user else None
-        )  # noqa: E501
+        partner = request.env.user.partner_id if request.env.user else False
         registration_disabled = False
         error_message = ""
 
-        if partner_id:
-            # Tarkistetaan, onko käyttäjällä
-            # jo ilmoittautuminen samaan koetyyppiin
-            existing_registration = (
-                request.env["event.registration"]
-                .sudo()
-                .search(
-                    [
-                        (
-                            "registration_survey_id",
-                            "in",
-                            event.sudo().survey_ids.ids,
-                        ),  # noqa: E501
-                        ("partner_id", "=", partner_id),
-                        ("state", "in", ["open"]),
-                        ("event_id.stage_id.pipe_end", "=", False),
-                    ]
-                )
-            )
+        if partner:
+            user_dog_ids = partner.my_dog_ids.ids
 
-            if existing_registration:
-                registration_disabled = True
-                error_message = (
-                    "Olet jo ilmoittautunut tähän koetyyppiin. "
-                    "Odota arviointia ennen uutta ilmoittautumista."
+            if user_dog_ids:
+                existing_registrations = (
+                    request.env["event.registration"]
+                    .sudo()
+                    .search(
+                        [
+                            (
+                                "registration_survey_id",
+                                "in",
+                                event.sudo().survey_ids.ids,
+                            ),
+                            (
+                                "registration_partner_id",
+                                "in",
+                                user_dog_ids,
+                            ),
+                            ("state", "in", ["open"]),
+                            ("event_id.stage_id.pipe_end", "=", False),
+                        ]
+                    )
                 )
+
+                registered_dog_ids = set(
+                    existing_registrations.mapped(
+                        "registration_partner_id"
+                    ).ids
+                )
+
+                if set(user_dog_ids).issubset(registered_dog_ids):
+                    registration_disabled = True
+                    error_message = (
+                        "Kaikki koirasi ovat jo ilmoittautuneet tähän "
+                        "koetyyppiin. Odota arviointia ennen uutta "
+                        "ilmoittautumista."
+                    )
+            else:
+                registration_disabled = True
+                error_message = "Sinulla ei ole koiria lisättynä."
 
         values = super(
             CustomWebsiteEventRegistrationController, self
-        ).event_register(  # noqa: E501
-            event, **post
-        )
+        ).event_register(event, **post)
+
         values.qcontext.update(
             {
                 "registration_disabled": registration_disabled,
